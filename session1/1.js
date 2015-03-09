d46765b776b5a3b4450ae582729b714f4d4fec4b
gabe <GabeMaker@users.noreply.github.com>
2015-03-09 23:27:16 +0000

    session 1-1 (not tested)

---
+++ session1/1.js
@@ -0,0 +1,9 @@
+// Given a number, return 20 less than, that number multiplied by 5
+
+// arithmetic1(10)     # => 30
+// arithmetic1(10.5)   # => 32.5
+// arithmetic1(-6)     # => -50
+
+var arithmetic1 = function (n) {
+  return (n * 5) - 20;
+};
