f0d380b9b81565aa0810a7e89c6a4c5c7236bf18
gabe <GabeMaker@users.noreply.github.com>
2015-03-11 21:29:11 +0000

    clear directory

--- session1/2.js
+++
@@ -1,9 +0,0 @@
-// Given two numbers, a and b, return half of whichever is smallest, as a float
-
-var arithmetic2 = function (a, b){
-  var smallest = a;
-  if (a > b) {
-    smallest = b;
-  }
-  return smallest / 2.0;
-};
--- spec/1Spec.js
+++
@@ -1,7 +0,0 @@
-describe ("arithmetic1", function() {
-
-  it("should return 30 when given 10", function() {
-    expect(arithmetic1(10)).toBe 30;
-  });
-
-});
