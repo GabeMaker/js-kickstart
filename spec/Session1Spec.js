ed48dcce0791849e724bdf47f6b205ce657dab1a
gabe <GabeMaker@users.noreply.github.com>
2015-03-12 10:08:03 +0000

    1-3 complete

--- spec/Session1Spec.js
+++ spec/Session1Spec.js
@@ -34,3 +34,29 @@ describe("Testing jasmine setup", function() {
     });
   });
 });
+
+  describe("1-3 ten_twenty", function() {
+    it("returns 20 if number given is odd", function() {
+      expect(ten_twenty(5)).toEqual(20);
+    });
+
+    it("returns 10 if number given is even", function() {
+      expect(ten_twenty(6)).toEqual(10);
+    });
+
+  });
+
+
+
+// # remember, you can test this file with
+// #   $ rake 1:3
+
+
+// # Given a number, n, return 10 if it is even, and 20 if it is odd
+// #
+// # ten_twenty(5) # => 20
+// # ten_twenty(6) # => 10
+
+// def ten_twenty(n)
+//   # your code goes here
+// end
--- src/Session1.js
+++ src/Session1.js
@@ -5,8 +5,15 @@ var arithmetic1 = function(n) {
 var arithmetic2 = function (x, y) {
   if (x < y) {
     return x / 2;
-  }
-  else {
+  } else {
     return y / 2;
   }
+};
+
+var ten_twenty = function (n) {
+  var result = 10
+  if (n % 2 !== 0) {
+    result = 20;
+  }
+  return result;
 };
