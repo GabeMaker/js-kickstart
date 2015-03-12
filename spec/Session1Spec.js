4c636970b43d8be393cb85bb117c55646d496ae5
gabe <GabeMaker@users.noreply.github.com>
2015-03-12 09:40:53 +0000

    1-2 complete

--- spec/Session1Spec.js
+++ spec/Session1Spec.js
@@ -4,7 +4,8 @@ describe("Testing jasmine setup", function() {
     expect(1 + 1).toBe(2);
   });
 
-  describe("arithmetic1", function() {
+
+  describe("1-1 arithmetic1", function() {
     it("returns 20 less than input multiplied by 5", function() {
       expect(arithmetic1(10)).toBe(30);
     });
@@ -18,4 +19,18 @@ describe("Testing jasmine setup", function() {
     });
   });
 
+
+  describe("1-2 arithmetic2", function() {
+    it("returns half the smallest given parameter/argument", function() {
+      expect(arithmetic2(1,2)).toBe(0.5);
+    });
+
+    it("returns 5.0 when the parameters are 19 and 10", function() {
+      expect(arithmetic2(19,10)).toBe(5.0);
+    });
+
+    it("returns -3.5 when the parameters are -6 and -7", function() {
+      expect(arithmetic2(-6, -7)).toBe(-3.5);
+    });
+  });
 });
--- src/Session1.js
+++ src/Session1.js
@@ -1,3 +1,12 @@
 var arithmetic1 = function(n) {
   return n * 5 - 20;
 };
+
+var arithmetic2 = function (x, y) {
+  if (x < y) {
+    return x / 2;
+  }
+  else {
+    return y / 2;
+  }
+};
